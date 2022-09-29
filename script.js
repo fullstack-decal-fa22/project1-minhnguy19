ad7a6d954bad2cdae3ca8e148b2735b4be451977
Minh Nguyen <minhnguy19@berkeley.edu>
2022-09-28 19:33:25 -0700

    i finished project1

---
+++ script.js
@@ -0,0 +1,13 @@
+function validate() {
+    var button_show = document.getElementById("link-button");
+    var answer  = document.getElementById("answer");
+      
+  if(answer){
+     if(answer.value  == "yes"){
+           button_show.style.display = "block";
+     }
+   }
+}
+
+function visitCorgi()
+{ window.location="https://corgiorgy.com/"; }
